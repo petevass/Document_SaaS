30cfd6c2014d4d2fc2ea125974375c98ba975f9e
petevass <pvassiliou10@gmail.com>
2026-09-05 09:24:33 -0400

    initial commit to repo

---
+++ src/main/java/com/mycompany/app/newgoogldocs/NewGooglDocsApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.newgoogldocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewGooglDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewGooglDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mycompany/app/newgoogldocs/NewGooglDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.app.newgoogldocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewGooglDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
